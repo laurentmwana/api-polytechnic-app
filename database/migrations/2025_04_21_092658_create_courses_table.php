15a87cea61468a37cda5b938801da68afba10708
Laurent Mwanamputu <laurentmwn@gmail.com>
2025-04-21 14:17:58 +0100

    feat: migration for student, etc.

---
+++ database/migrations/2025_04_21_092658_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('alias');
+            $table->integers('credits')->default(1);
+            $table->foreignId('professor_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('level_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
